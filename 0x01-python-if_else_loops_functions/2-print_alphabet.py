b144e9c70a99750ee97cd46a2db61feb286134f4
ndouishmael <111059267+ndouishmael@users.noreply.github.com>
2022-11-29 17:53:06 +0200

    task 2 fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
-
-def print_low_alpha():
-    for c in 'abcdefghijklmnopqrstuvwxyz':
-        print('{}'.format(c), end='')
-
-if __name__ == '__main__':
-    print_low_alpha()
+for c in range(ord('a'), ord('z')+1):
+    print('{}'.format(chr(c)), end='')
