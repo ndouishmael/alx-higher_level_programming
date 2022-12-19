ada0369f643836c013ea3146346a0daf5500c745
ndouishmael <111059267+ndouishmael@users.noreply.github.com>
2022-12-19 17:58:32 +0200

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
+
+
 def safe_print_division(a, b):
-    quotient = None
+    quotient = 0
+
     try:
         quotient = a / b
-    except:
-        pass
+    except ZeroDivisionError:
+        quotient = None
+    except TypeError:
+        quotient = None
     finally:
-        if quotient is not None:
-            print("Inside result: {:.1f}".format(quotient))
-        else:
-            print("Inside result: None")
+        print('Inside result: {}'.format(quotient))
+
     return quotient
+
+
+if __name__ == '__main__':
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
