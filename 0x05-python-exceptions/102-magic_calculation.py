97d3ddab11680366e3330ab68b0cf3c7cd48d243
ndouishmael <111059267+ndouishmael@users.noreply.github.com>
2022-12-19 18:12:57 +0200

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,2 +1,13 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
-    return (98 + a ** b)
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result = result + (a**b) / i
+        except:
+            result = b + a
+            break
+    return (result)
