a6d933b816c8a93f0f15ca01ecc2a75b7044c765
ndouishmael <111059267+ndouishmael@users.noreply.github.com>
2022-12-20 13:30:33 +0200

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
-'''
-Square class definition
-'''
 
 
-class Square():
-    '''
-    Square class with private attribute size
-    '''
-    def __init__(self, size=0):
-        '''
+class Square:
+    """Square class."""
+
+    def __init__(self, size):
+        """__init__ method for Square.
         Args:
-           size: size of the square
-        '''
-        self.__size = 
+            size (int): size of Square.
+        """
+        self.__size = size
+
+
+if __name__ == '__main__':
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
