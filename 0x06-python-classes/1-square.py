efee15efcb15134dcb3902d4f7b5594045fdc706
ndouishmael <111059267+ndouishmael@users.noreply.github.com>
2022-12-20 13:29:18 +0200

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Square class definition
+'''
+
+
+class Square():
+    '''
+    Square class with private attribute size
+    '''
+    def __init__(self, size=0):
+        '''
+        Args:
+           size: size of the square
+        '''
+        self.__size = 
