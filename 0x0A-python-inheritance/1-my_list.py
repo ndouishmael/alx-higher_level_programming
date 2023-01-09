e76322c6969f38934e3a213529aa6aae0f18cf71
ndouishmael <111059267+ndouishmael@users.noreply.github.com>
2023-01-09 12:55:10 +0200

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-# 1-my_list.py
-"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """Implements sorted printing for the built-in list class."""
-
     def print_sorted(self):
-        """Print a list in sorted ascending order."""
-        print(sorted(self))
+        """Use bubble sort to create a sorted list to print
+        Args:
+            self (list): unsorted list
+        """
+        arr = self[:]
+        done = False
+        while not done:
+            done = True
+            for i in range(len(arr)-1):
+                if arr[i] > arr[i+1]:
+                    arr[i], arr[i+1] = arr[i+1], arr[i]
+                    done = False
+        print(arr)
+
+if __name__ == '__main__':
+    my_list = MyList()
+    my_list.append(1)
+    my_list.append(4)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.append(5)
+    print(my_list)
+    my_list.print_sorted()
+    print(my_list)
