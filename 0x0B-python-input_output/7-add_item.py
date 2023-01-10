dbe2710169d9c6b26c6d7aeba85674520138b6d0
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-01-10 18:55:43 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-import os
+# 7-add_item.py
+# Brennan D Baraban <ndouishmael@gmail.com>
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-import json
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
-filename = 'add_item.json'
-args = len(sys.argv)
-
-if not os.path.isfile(filename):
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write('[]')
-
-if args > 1:
-    data = load_from_json_file(filename)
-    for i in range(1, args):
-        data.append(sys.argv[i])
-    save_to_json_file(data, filename)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
