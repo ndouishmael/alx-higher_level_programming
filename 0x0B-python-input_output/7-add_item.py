d09658c9065af48656d52148edea532316cb26ff
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-01-10 18:51:08 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-# 7-add_item.py
-"""Add all arguments to a Python list and save them to a file."""
-import sys
+'''
+Script that adds all arguments to a Python list and then
+saves them to a file
+'''
+from sys import argv
+import json
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
+    filename = 'add_item.json'
 
     try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+        new = load_from_json_file(filename)
+    except (ValueError, FileNotFoundError):
+        new = []
+
+    new = new + argv[1:]
+    save_to_json_file(new, filename)
