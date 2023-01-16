f57659f31da5c218b882f8969a45363e79ad802c
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-01-16 12:31:20 +0200

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # test_base.py
-# Brennan D Baraban <375@holbertonschool.com>
 """Defines unittests for base.py.
+
 Unittest classes:
     TestBase_instantiation - line 23
     TestBase_to_json_string - line 110
