cda6cfdb042ff1dcb53b47cc83753eae29507774
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-02-15 20:29:47 +0200

    Update 3-force_name.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,9 +1,5 @@
--- Check if the table exists
-USE database_name;
-SELECT * FROM information_schema.tables WHERE table_name = 'force_name' LIMIT 1;
-
--- Create the table if it doesn't exist
-CREATE TABLE IF NOT EXISTS force_name (
-  id INT,
-  name VARCHAR(256) NOT NULL
+-- Creates the table force_name.
+CREATE TABLE IF NOT EXISTS `force_name` (
+    `id`   INT,
+    `name` VARCHAR(256) NOT NULL
 );
