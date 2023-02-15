6e1ad51b33c9ad3f994de4b9af05df45695830a8
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-02-15 20:12:07 +0200

    Create 1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create the user if it doesn't already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
