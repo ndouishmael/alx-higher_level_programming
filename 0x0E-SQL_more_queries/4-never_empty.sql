dd1af4646ab6f42f7bdd19229ed0723cb31e30fd
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-02-15 20:33:18 +0200

    Update 4-never_empty.sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,9 +1,5 @@
--- Check if the table exists
-USE database_name;
-SELECT * FROM information_schema.tables WHERE table_name = 'id_not_null' LIMIT 1;
-
--- Create the table if it doesn't exist
-CREATE TABLE IF NOT EXISTS id_not_null (
-  id INT DEFAULT 1,
-  name VARCHAR(256)
+-- Creates the table id_not_null
+CREATE TABLE IF NOT EXISTS `id_not_null` (
+    `id`   INT          DEFAULT 1,
+    `name` VARCHAR(256)
 );
