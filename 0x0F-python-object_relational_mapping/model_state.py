be2f7647a229faed9c08df4e0af5d39346e737c6
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-03-17 18:01:51 +0200

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,14 +2,16 @@
 """Contains the class definition of a State and an instance
    Base = declarative_base()
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
 
 Base = declarative_base()
 
-
 class State(Base):
     __tablename__ = 'states'
-    id = Column(Integer, nullable=False, unique=True, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+engine = create_engine('mysql+pymysql://root@localhost:3306/MySQL serverb')
+
+Base.metadata.create_all(engine)
