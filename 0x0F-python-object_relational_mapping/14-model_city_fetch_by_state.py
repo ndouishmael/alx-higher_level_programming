6310e6270e53830f8c2cc25ba2c631338e916f5d
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-03-17 18:32:41 +0200

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,20 +3,20 @@
    takes arguments: mysql-username, mysql-password, database-name
 """
 
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
+
 if __name__ == "__main__":
-    import sys
-    from sqlalchemy import (create_engine)
-    from sqlalchemy.orm import sessionmaker
-    from model_state import Base, State
-    from model_city import City
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state, city in session.query(State, City).\
-            filter(State.id == City.state_id).order_by(City.id).all():
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
         print("{}: ({}) {}".format(state.name, city.id, city.name))
