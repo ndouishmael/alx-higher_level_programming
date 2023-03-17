477d0189b76a75405ee5c3546b64d9ff44feff84
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-03-17 19:33:21 +0200

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
+"""Contains the class definition of a State and an instance
+   Base = declarative_base()
+"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
