2e6b1ec2c815c89d62b6aa9247c5e3000d595917
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-03-30 19:07:31 +0200

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL as an argument, sends a GET request to the URL
-curl -H "X-HolbertonSchool-User-Id:98" -sLX GET "$1"
+curl -H "X-School-User-Id:98" -sLX GET "$1"
