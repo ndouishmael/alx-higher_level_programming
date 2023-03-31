03dbd3424c8460419d728b41405df7865582b747
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-03-31 22:33:11 +0200

    Create 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" script fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode(encoding='utf-8')))
