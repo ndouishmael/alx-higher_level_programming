af30ced7cb1ea45c13617a1490f31d811cd65ec1
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-03-31 22:42:00 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ script fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
+
+
 if __name__ == '__main__':
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
