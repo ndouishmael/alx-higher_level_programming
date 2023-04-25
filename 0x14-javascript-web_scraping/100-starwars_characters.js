439213487ba8d245465a252171dd4e61b927ab72
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-04-25 22:43:20 +0200

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,28 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
+
+const options = {
+  url: 'http://swapi.co/api/films/' + process.argv[2],
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+function callback (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const chars = data['characters'];
+    let i;
+    for (i = 0; i < chars.length; i++) {
+      request(chars[i], function (error, response, body) {
+        if (!error && response.statusCode === 200 && body) {
+          const cdata = JSON.parse(body);
+          console.log(cdata['name']);
         }
       });
-    });
+    }
   }
-});
+}
+
+request(options, callback);
