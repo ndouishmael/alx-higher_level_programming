634b7fa36c74dc7af6a34ddb3619dd7bab2a8848
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-04-25 22:25:33 +0200

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,32 +3,17 @@
 'use strict';
 
 const request = require('request');
-
-const apiUrl = process.argv[2];
-
-request(apiUrl, function (error, response, body) {
-if (error) {
-console.error(error);
-return;
-}
-
-if (response.statusCode !== 200) {
-console.error('Invalid status code:', response.statusCode);
-return;
-}
-
-const todos = JSON.parse(body);
-
-const users = {};
-
-for (const todo of todos) {
-if (todo.completed) {
-const userId = todo.userId;
-users[userId] = (users[userId] || 0) + 1;
-}
-}
-
-for (const userId in users) {
-console.log(${userId}: ${users[userId]} tasks completed);
-}
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    let completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
+    });
+    console.log(completed);
+  }
 });
