f99ea621d43427524253fac7f85246d4f6cfdd50
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-04-25 21:27:48 +0200

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,19 @@
 #!/usr/bin/node
+
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
-});
+
+const options = {
+  url: 'http://swapi.co/api/films/' + process.argv[2],
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+function callback (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const info = JSON.parse(body);
+    console.log(info['title']);
+  }
+}
+
+request(options, callback);
